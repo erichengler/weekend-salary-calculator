0d725dde419232efb3acfed0f1ae76cee1055d0d
Erich Engler <erich.engler@hotmail.com>
2023-02-14 21:47:42 -0600

    added a working delete button

--- client.js
+++ client.js
@@ -8,17 +8,37 @@ function submitForm( event ) {
 
     let firstNameVal = document.querySelector( '#first-name-input' ).value
     let lastNameVal = document.querySelector( '#last-name-input' ).value
-    let idNumberVal = document.querySelector( '#id-number-input' ).value
+    let idNumberVal = Number(document.querySelector( '#id-number-input' ).value)
     let jobTitleVal = document.querySelector( '#job-title-input' ).value
-    let salaryVal = document.querySelector( '#salary-input ').value
+    let salaryVal = Number(document.querySelector( '#salary-input ').value)
 
-    employeeInfoArray.push(
-        {
-            FirstName: firstNameVal,
-            LastName: lastNameVal,
-            ID: idNumberVal,
-            JobTitle: jobTitleVal,
-            Salary: salaryVal
-        }
-    );
+    let newEmployee = {
+        FirstName: firstNameVal,
+        LastName: lastNameVal,
+        ID: idNumberVal,
+        JobTitle: jobTitleVal,
+        Salary: salaryVal
+    }
+
+    employeeInfoArray.push( newEmployee );
+    console.log( employeeInfoArray );
+
+    // Appending employee information values to the table
+    let appendTable = document.querySelector( '#appendMe' );
+    appendTable.innerHTML += `
+        <tr>
+            <td>${firstNameVal}</td>
+            <td>${lastNameVal}</td>
+            <td>${idNumberVal}</td>
+            <td>${jobTitleVal}</td>
+            <td>${salaryVal}</td>
+            <td><button id="delete-button" onClick="deleter(event)">Delete</button>
+        </tr>    
+    `;
+}
+
+// Removing employee row from table
+function deleter( event ) {
+    event.target.parentElement.parentElement.remove();
+
 }
